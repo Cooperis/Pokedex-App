98c388033dcb60bea954b339eddf4b8cfcc28734
Cooperis <tomas.kuprys1997@gmail.com>
2021-07-08 22:56:49 +0200

    Added an array of Pokemons and attributes

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+let pokemonList = [{name: "Charizard", height: 5, type: ['Fire', 'Flying']},{name: "Ivysaur", height: 3, type: ['Poison', 'Grass']}, {name: "Pidgeot", height: 4, type: ['Normal', 'Flying']}];
