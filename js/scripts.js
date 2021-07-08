4988d73c04172d3e207098a9fedd1d346af42893
Cooperis <tomas.kuprys1997@gmail.com>
2021-07-08 23:00:50 +0200

    Added formatting for readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [{name: "Charizard", height: 5, type: ['Fire', 'Flying']},{name: "Ivysaur", height: 3, type: ['Poison', 'Grass']}, {name: "Pidgeot", height: 4, type: ['Normal', 'Flying']}];
+let pokemonList = [
+  {name: "Charizard", height: 5, type: ['Fire', 'Flying']},
+  {name: "Ivysaur", height: 3, type: ['Poison', 'Grass']},
+  {name: "Pidgeot", height: 4, type: ['Normal', 'Flying']}
+];
