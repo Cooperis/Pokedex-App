c0ddd3f0568ca98608d78577f34720c5858de73b
Cooperis <tomas.kuprys1997@gmail.com>
2021-07-12 16:20:20 +0200

    Printed out the list, attributes with a conditional on the website screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 let pokemonList = [
-  {name: "Charizard", height: 5, type: ['Fire', 'Flying']},
-  {name: "Ivysaur", height: 3, type: ['Poison', 'Grass']},
-  {name: "Pidgeot", height: 4, type: ['Normal', 'Flying']}
+  {
+    name: "Charizard",
+    height: 5,
+    type: ["Fire", "Flying"]
+  },
+  {
+    name: "Ivysaur",
+    height: 3,
+    type: ["Poison", "Grass"]
+  },
+  {
+    name: "Pidgeot",
+    height: 4,
+    type: ["Normal", "Flying"]
+  }
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+  if(pokemonList[i].height>=5) {
+    document.write (" - Wow, that's a big Pokemon!" + "<br>");
+  } else {
+    document.write ("<br>");
+  }
+}
