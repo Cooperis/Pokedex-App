1d79b8b4f06e1ec39a60c76728954f5739d4f8d7
Cooperis <tomas.kuprys1997@gmail.com>
2021-08-24 23:42:29 +0200

    Changed individual for functions to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,11 @@ let pokemonList = [
   }
 ];
 
-for (let i=0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-  if(pokemonList[i].height>=5) {
+pokemonList.forEach (function(pokemon) {
+  document.write(pokemon.name + " (height: " + pokemon.height + ")");
+  if(pokemon.height>=5) {
     document.write (" - Wow, that's a big Pokemon!" + "<br>");
   } else {
     document.write ("<br>");
   }
-}
+});
