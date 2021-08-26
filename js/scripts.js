5237f95a3f7230e5825281c0ae7f2e809e6c0bc7
Cooperis <tomas.kuprys1997@gmail.com>
2021-08-26 23:02:14 +0200

    Wrapped pokemonList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
-let pokemonList = [
-  {
-    name: "Charizard",
-    height: 5,
-    type: ["Fire", "Flying"]
-  },
-  {
-    name: "Ivysaur",
-    height: 3,
-    type: ["Poison", "Grass"]
-  },
-  {
-    name: "Pidgeot",
-    height: 4,
-    type: ["Normal", "Flying"]
-  }
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Charizard",
+      height: 5,
+      type: ["Fire", "Flying"]
+    },
+    {
+      name: "Ivysaur",
+      height: 3,
+      type: ["Poison", "Grass"]
+    },
+    {
+      name: "Pidgeot",
+      height: 4,
+      type: ["Normal", "Flying"]
+    }
+  ];
 
-pokemonList.forEach (function(pokemon) {
+  function getAll() {
+    return pokemonList;
+  }
+  
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    add:add,
+    getAll:getAll
+  };
+})();
+
+let newPokemonList = pokemonRepository.getAll();
+
+newPokemonList.forEach (function(pokemon) {
   document.write(pokemon.name + " (height: " + pokemon.height + ")");
   if(pokemon.height>=5) {
     document.write (" - Wow, that's a big Pokemon!" + "<br>");
